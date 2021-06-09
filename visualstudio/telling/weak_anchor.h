93aed978235f8807bc91f81ad6e195b56eb04d8d
EvanBalster <evan@interactopia.com>
2021-06-08 22:09:36 -0700

    weak_anchor grooming

--- visualstudio/telling/weak_anchor.h
+++ visualstudio/telling/weak_anchor.h
@@ -3,15 +3,19 @@
 
 #include <memory>
 #include <thread>
+#include <atomic>
 
 
 /*
-	weak_anchor is used to produce weak_ptr to a non-heap object.
-		A shared_ptr derived from these will block the anchor's destruction.
+	weak_anchor is used to produce weak pointers to an object that isn't necessarily
+		allocated with new or make_shared.  Shared pointers created from these
+		weak pointers will block the object's destruction until they are released.
+
+	The user should take care to lock these pointers only briefly lest the program deadlock.
 	
-	This class was designed for use with asynchronous I/O delegates.
-		It is particularly lightweight, using a timed backoff spinlock for synchronization.
-		Consequently, the user should be careful not to retain references too long.
+	This class was designed for use with asynchronous I/O delegates, allowing observers
+		to be destroyed at will *unless* a callback happens to be occurring.
+		The one-time lock mechanism used here is much more lightweight than a mutex.
 */
 
 
@@ -21,11 +25,17 @@ namespace telling
 	class weak_anchor
 	{
 	private:
-		struct noop_deleter {void operator()(T *ptr) const noexcept {}};
+		struct deleter
+		{
+			std::atomic<std::atomic_flag*> keep_alive;
+
+			deleter() : keep_alive(nullptr) {}
+			void operator()(T *ptr) const noexcept    {keep_alive.load()->clear();}
+		};
 
 
 	public:
-		weak_anchor(T *ptr)        : expire(0), hold(std::unique_ptr<T,noop_deleter>(ptr, {})) {}
+		weak_anchor(T *ptr)        : expire(0), hold(std::unique_ptr<T,deleter>(ptr, {})) {}
 		~weak_anchor() noexcept    {destroy();}
 
 		/*
@@ -41,15 +51,18 @@ namespace telling
 		{
 			if (hold)
 			{
-				std::weak_ptr<T> weak = hold;
+				// Create a "keep alive" flag; when this is unset, destruction can complete
+				std::atomic_flag keep_alive;
+				keep_alive.test_and_set(std::memory_order_release);
+				std::get_deleter<deleter>(hold)->expire.store(&expire);
 				hold.reset();
 
-				// Spin 65,536 times
-				for (size_t i = 0; i < 0xFFFF; ++i)
-					if (weak.expired()) return;
+				// Spin 16,192 times
+				for (size_t i = 0; i < 0x4000; ++i)
+					if (!keep_alive.test_and_set(std::memory_order_acquire)) return;
 
 				// Exponential backoff from .000001 to ~.26 seconds
-				for (size_t i = 0; !weak.expired(); ++i)
+				for (size_t i = 0; keep_alive.test_and_set(std::memory_order_acquire); ++i)
 					std::this_thread::sleep_for(std::chrono::microseconds(1 << std::min<size_t>(i, 18)));
 			}
 		}
